a2b22a7b1cd205ff0662bb7b3b14ba4fb8a3dba7
Kpaiy <kpaiy42@gmail.com>
2025-03-10 23:16:04 +1100

    Use UI Toolkit to display sprites
    This is currently just a test canvas, I'll need to
    extend it to programmatically add/remove elements.

--- RumbleHud/Core.cs
+++ RumbleHud/Core.cs
@@ -1,8 +1,11 @@
 ﻿using Il2CppRUMBLE.Managers;
+using Il2CppSystem.Security.Cryptography;
 using Il2CppTMPro;
 using MelonLoader;
 using System.Reflection.Metadata;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 [assembly: MelonInfo(typeof(RumbleHud.Core), "RumbleHud", "0.1.0", "Kpaiy", null)]
 [assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
@@ -24,7 +27,11 @@ namespace RumbleHud
         private List<PlayerInfo> playerInfos = null;
 
         private Font font = null;
-        private Texture background = null;
+        private Texture2D background = null;
+
+        private GameObject uiContainer = null;
+        private Canvas canvas = null;
+        private Text text = null;
 
         public override void OnInitializeMelon()
         {
@@ -36,9 +43,51 @@ namespace RumbleHud
             var bundle = Il2CppAssetBundleManager.LoadFromFile(@"UserData/rumblehud");
             // GameObject myGameObject = GameObject.Instantiate(bundle.LoadAsset<GameObject>("Object name goes here!"));
             font = GameObject.Instantiate(bundle.LoadAsset<Font>("GoodDogPlain"));
-            background = GameObject.Instantiate(bundle.LoadAsset<Texture2D>("HudBackground"));
-        }
+            background = GameObject.Instantiate(bundle.LoadAsset<Texture2D>("PlayerBackground"));
 
+            uiContainer = new GameObject();
+            uiContainer.name = "Canvas";
+            uiContainer.AddComponent<Canvas>();
+
+            canvas = uiContainer.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            uiContainer.AddComponent<CanvasScaler>();
+            uiContainer.AddComponent<GraphicRaycaster>();
+
+            LoggerInstance.Msg($@"RumbleHud: ${canvas.pixelRect.ToString()}");
+
+            GameObject myText = new GameObject();
+            myText.transform.parent = uiContainer.transform;
+            myText.name = "wibble";
+
+            text = myText.AddComponent<Text>();
+            text.font = font;
+            text.text = "wobble";
+            text.fontSize = 100;
+
+            GameObject imageObject = new GameObject();
+            imageObject.transform.parent = uiContainer.transform;
+            imageObject.name = "background";
+
+            RawImage image = imageObject.AddComponent<RawImage>();
+            image.texture = background;
+            image.SetNativeSize();
+
+            var imageTransform = image.GetComponent<RectTransform>();
+            // imageTransform.sizeDelta = new Vector2(background.width, background.height);
+            imageTransform.anchorMin = new Vector2(0, 1);
+            imageTransform.anchorMax = new Vector2(0, 1);
+            imageTransform.pivot = new Vector2(0, 1); // Pivot on top left.
+            imageTransform.anchoredPosition = new Vector3(0, -100, 0);
+
+            LoggerInstance.Msg($@"RumbleHud: ${imageTransform.position}");
+            LoggerInstance.Msg($@"RumbleHud: ${imageTransform.anchoredPosition}");
+
+            // Text position
+            var rectTransform = text.GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(0, 0, 0);
+            rectTransform.sizeDelta = new Vector2(400, 200);
+        }
 
         public override void OnUpdate()
         {
@@ -85,6 +134,8 @@ namespace RumbleHud
 
         public override void OnGUI()
         {
+            base.OnGUI(); return;
+
             if (font == null || playerInfos == null) return;
 
             GUI.skin.font = font;
