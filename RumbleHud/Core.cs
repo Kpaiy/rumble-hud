b164f538af8ba7f816fcc6c948ed67473125267b
Kpaiy <kpaiy42@gmail.com>
2025-03-10 13:08:44 +1100

    Initial commit

---
+++ RumbleHud/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(RumbleHud.Core), "RumbleHud", "0.1.0", "Kpaiy", null)]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+
+namespace RumbleHud
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("RumbleHud: Initialized.");
+        }
+    }
+}
